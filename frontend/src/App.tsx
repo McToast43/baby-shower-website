1b7cfe034f0aaa3332d74c05e936a577d49ca08b
Benjamin Jönsson <benjamin.jonsson@geshdo.com>
2024-10-12 16:01:35 +0200

    Added frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import Placeholder from "./components/placeholder";
+
+function App() {
+  return Placeholder();
+}
+
+export default App;
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Arlenes Baby shower</h1>
+    </header>
+  );
+};
+
+export default Header;
