0bf4ecdf8f82c2fb25cdafa3f6ce7d0d286d5f0e
Benjamin Jönsson <benjamin.jonsson@geshdo.com>
2024-10-12 17:11:23 +0200

    added basic hero

--- frontend/src/components/Hero/Hero.tsx
+++ frontend/src/components/Hero/Hero.tsx
@@ -11,6 +11,21 @@ const Hero = () => {
   return (
     <div className="hero">
       <h1>Det blir en tös!!</h1>
+      <p>
+        Vi vill fira att det blir en tös och bjud in er till en liten Baby
+        Shower för att fira detta.
+        <br />
+        Det bjuds på tårta och mat. Ni bjuder på er själva och en present.
+      </p>
+      <p>Lördagen den 30:e November kl 14:00</p>
+      {/* Add countdown here :) */}
+      <p>
+        Här neranför ser ni en lista på saker som vi önskar oss. Om ni vill köpa
+        en present så klickar ni på den och bekräftar att ni köper den.
+        <br />
+        Om ni inte vill köpa något så är det helt okej, vi vill mest att ni
+        kommer och firar med oss.
+      </p>
     </div>
   );
 };
---
+++ frontend/src/components/Modals/ConfirmItem.tsx
@@ -0,0 +1,4 @@
+//Will show up when claiming a gift
+
+//Will show the name of the item and the name of the person who is claiming it
+//Options to confirm or cancel
